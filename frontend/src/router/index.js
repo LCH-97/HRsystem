531ffecb8c7516792418e8e169735b8458ed54cc
kje <kimjieun121314@naver.com>
2024-03-14 16:54:19 +0900

    [Fix] 초과 근무 프론트엔드 vue 변경 (#126)
    -초과 근무 html 파일들(overtimecreate, overtimeapprove,overtimemodify)을 vue로 바꿨습니다.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,9 @@ import SignUpPage from "../pages/SignUpPage.vue";
 import LoginPage from "../pages/LoginPage.vue";
 import MainPage from "../pages/MainPage.vue";
 import OvertimeCreatePage from '../pages/OvertimeCreatePage.vue';
+import OvertimeListPage from '../pages/OvertimeListPage.vue';
+import OvertimeModifyPage from '../pages/OvertimeModifyPage.vue';
+import OvertimeApproveaPage from '../pages/OvertimeApproveaPage.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,6 +17,9 @@ const router = createRouter({
     { path: "/login", component: LoginPage },
     { path: "/main", component: MainPage },
     { path: "/overtimecreate", component: OvertimeCreatePage},
+    { path: "/overtimelist", component: OvertimeListPage},
+    { path: "/overtimemodify", component: OvertimeModifyPage},
+    { path: "/overtimeapprovea", component: OvertimeApproveaPage},
   ],
 });
 
