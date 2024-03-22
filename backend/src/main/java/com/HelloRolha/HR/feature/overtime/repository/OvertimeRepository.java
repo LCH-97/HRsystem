e91cf4b8f1540522f8b8413ef27fa121ab59ae55
thanks9807 <thanks9807@naver.com>
2024-03-22 11:50:51 +0900

    [Remove] 충돌 해결을 위한 파일 삭제 - 문서 파일이 충돌이 발생하여 방해되는 백, 프론트엔드 파일 삭제

--- backend/src/main/java/com/HelloRolha/HR/feature/overtime/repository/OvertimeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.HelloRolha.HR.feature.overtime.repository;
-
-
-import com.HelloRolha.HR.feature.overtime.model.Overtime;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OvertimeRepository extends JpaRepository<Overtime, Integer> {
-}
--- backend/src/main/java/com/HelloRolha/HR/feature/position/repo/PositionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.HelloRolha.HR.feature.position.repo;
-
-import com.HelloRolha.HR.feature.employee.model.entity.Employee;
-import com.HelloRolha.HR.feature.position.model.entity.Position;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PositionRepository extends JpaRepository<Position, Integer> {
-}
