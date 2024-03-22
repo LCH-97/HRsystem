904e59a79ae00e9d04db88e5fe04bec23dee1831
thanks9807 <thanks9807@naver.com>
2024-03-22 12:58:00 +0900

    [Test] Ci 테스트

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,7 +3,8 @@ const config = {
     'default',
     ['jest-junit', {outputDirectory: 'test-results', outputName: 'report.xml'}],
   ],
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  transformIgnorePatterns: ["node_modules/(?!axios)"]
 };
 
 module.exports = config;
