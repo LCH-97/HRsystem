6ac476e82e8c75ceb969b242bdc76748eea1ea0a
Lee-Jun-Guk <ell0404@naver.com>
2024-03-25 19:48:37 +0900

    [Fix] 코드 구조 변경

--- backend/src/main/java/com/HelloRolha/HR/feature/approve/model/dto/Approve/ApproveCreateReq.java
+++ backend/src/main/java/com/HelloRolha/HR/feature/approve/model/dto/Approve/ApproveCreateReq.java
@@ -3,10 +3,6 @@ package com.HelloRolha.HR.feature.approve.model.dto.Approve;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Builder
 @Data
--- backend/src/main/java/com/HelloRolha/HR/feature/goout/model/dto/GooutRead.java
+++ backend/src/main/java/com/HelloRolha/HR/feature/goout/model/dto/GooutRead.java
@@ -1,3 +1,4 @@
+
 package com.HelloRolha.HR.feature.goout.model.dto;
 
 import lombok.Builder;
