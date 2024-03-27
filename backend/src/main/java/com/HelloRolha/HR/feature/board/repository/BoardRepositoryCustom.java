d1057515e885694f91b1784ada6354c5aa92ad41
kje <kimjieun121314@naver.com>
2024-03-27 12:32:05 +0900

    [Feat] 공지사항 쿼리 dsl 및 n+1 해결 (#218) -공지사항 기능 쿼리 dsl 및 n+1 문제를 해결

---
+++ backend/src/main/java/com/HelloRolha/HR/feature/board/repository/BoardRepositoryCustom.java
@@ -0,0 +1,16 @@
+package com.HelloRolha.HR.feature.board.repository;
+
+import com.HelloRolha.HR.feature.board.model.Board;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BoardRepositoryCustom {
+
+    public List<Board> findList();
+
+    Optional<Board> findByIdWithDetails(Integer id);
+
+
+    org.springframework.data.domain.Page<Board> findList(org.springframework.data.domain.Pageable pageable);
+}
