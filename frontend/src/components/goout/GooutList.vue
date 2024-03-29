9442e1bef6e29295f746e3925c1e2b3e1525f49f
LCH-97 <leechanghoon12@gmail.com>
2024-03-29 14:41:34 +0900

    [Feat] localhost:8080 > http://192.168.0.51/api 수정
    - 죄송합니다. 복붙하다가 그만..

--- frontend/src/components/goout/GooutList.vue
+++ frontend/src/components/goout/GooutList.vue
@@ -131,7 +131,7 @@ export default {
     },
     async fetchGoouts() {
       try {
-        const response = await axios.get(`http://localhost:8080/goout/check`, {
+        const response = await axios.get(`http://192.168.0.51/api/goout/check`, {
           params: {
             page: this.currentPage,
             size: this.pageSize,
