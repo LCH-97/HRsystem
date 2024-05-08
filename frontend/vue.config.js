2e0b852f37673f08241d929cb531ebadca5ba6d2
LCH-97 <leechanghoon12@gmail.com>
2024-05-08 16:12:23 +0900

    [Fix] vue.config.js 수정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  
-})
+
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        secure: false,
+        changeOrigin: true,
+        logLevel: 'debug'
+      }
+    }
+  }
+});
 
 // preset: "@vue/cli-plugin-unit-jest",
-//transformIgnorePatterns: ['/node_modules/']
+// transformIgnorePatterns: ['/node_modules/']
 // transformIgnorePatterns: ["node_modules/(?!axios)"],
